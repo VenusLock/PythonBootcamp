0f0f08bee830ae587da7cfd7d05d2b263b14e147
VenusLock <106877598+VenusLock@users.noreply.github.com>
2022-02-14 15:54:39 +0000

    footer style

--- device_info.cu
+++ device_info.cu
@@ -26,3 +26,4 @@ int main() {
     printf("  Peak Memory Bandwidth (GB/s): %f\n", 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
 
     printf("  Max Threads Per Block: %d\n", prop.maxThreadsPerBlock);
+  

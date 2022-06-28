b01546430188ecf634518241ee18a00d6cabe7c7
VenusLock <106877598+VenusLock@users.noreply.github.com>
2022-06-28 18:11:43 +0000

    changed the name

--- intro_to_cuda_1/add.cpp
+++ intro_to_cuda_1/add.cpp
@@ -13,3 +13,7 @@ int main(void)
     int N = 1 << 20; // ~1M elements, more preciesly 2^20 = 1048576 elements.:)
     float *x = new float[N];
     float *y = new float[N];
+
+    // init x and y arrays on the host
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;

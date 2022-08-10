609792e15daf72ef3eced64f7fbe512e37dbeef3
VenusLock <106877598+VenusLock@users.noreply.github.com>
2022-08-10 15:50:40 +0000

    Clean-ups

--- julia_set_2/julia_set.cpp
+++ julia_set_2/julia_set.cpp
@@ -47,3 +47,6 @@ void kernel(unsigned char *ptr) {
             ptr[offset*4 + 0] = 255 * juliaVal;
             ptr[offset*4 + 1] = 0;
             ptr[offset*4 + 2] = 0;
+            ptr[offset*4 + 3] = 255;
+
+        }

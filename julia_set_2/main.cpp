994b01b6f3833c17718e2496b7b2e220c72189dc
VenusLock <106877598+VenusLock@users.noreply.github.com>
2022-09-25 11:57:14 +0000

    Add privacy policy

--- julia_set_2/main.cpp
+++ julia_set_2/main.cpp
@@ -2,3 +2,6 @@
 #include <GL/glew.h> // include GLEW and new version of GL on Windows
 #include <GLFW/glfw3.h> // GLFW helper library for window management
 #include <iostream> //for cout
+
+int main (int argc, char** argv) 
+{

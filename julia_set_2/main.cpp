9622aca87c334916b4ac0b1b340a224775d009b0
VenusLock <106877598+VenusLock@users.noreply.github.com>
2022-10-01 20:42:42 +0000

    Initial Check-In

--- julia_set_2/main.cpp
+++ julia_set_2/main.cpp
@@ -13,3 +13,6 @@ int main (int argc, char** argv)
     } 
 
   //Setting window properties
+  glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
